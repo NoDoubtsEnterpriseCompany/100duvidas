627cf7d67b1d3332328819796885e661e7e0fac2
tiaraju <tiarajums@gmail.com>
2014-12-01 17:23:49 -0300

    criando tela inicial dos perfis de aluno e professor de acordo com o mock

--- gen/com/nodoubts/R.java
+++ gen/com/nodoubts/R.java
@@ -22,21 +22,46 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int avatar=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000b;
+        public static final int adress_text_field=0x7f08000a;
+        public static final int courses_text_field=0x7f080008;
+        public static final int editText1=0x7f080004;
+        public static final int email=0x7f080003;
+        public static final int email_text_field=0x7f080007;
+        public static final int img_view_teacher=0x7f080006;
+        public static final int loginbtn=0x7f080001;
+        public static final int password=0x7f080002;
+        public static final int ratingBar1=0x7f080009;
+        public static final int registerbtn=0x7f080000;
+        public static final int textView1=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_student_profile=0x7f030001;
+        public static final int activity_teacher_profile=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int student_profile=0x7f070001;
+        public static final int teacher_profile=0x7f070002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050004;
+        public static final int address=0x7f050009;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int email_hint=0x7f050008;
+        public static final int hello_world=0x7f050003;
+        public static final int login_message=0x7f050005;
+        public static final int offered_courses=0x7f05000a;
+        public static final int password_hint=0x7f050007;
+        public static final int register_message=0x7f050006;
+        public static final int starred_teachers=0x7f05000b;
+        public static final int title_activity_student_profile=0x7f050001;
+        public static final int title_activity_teacher_profile=0x7f050002;
     }
     public static final class style {
         /** 
---
+++ src/com/nodoubts/core/User.java
@@ -0,0 +1,13 @@
+package com.nodoubts.core;
+
+public class User {
+	
+	 String login,password,email;
+	
+	public User(String login,String password,String email){
+		this.login=login;
+		this.password=password;
+		this.email=email;
+	}
+
+}
