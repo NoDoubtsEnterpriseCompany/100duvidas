fcef3e1036a690889cb03cb760910c9e72ae2c5e
tiaraju <tiarajums@gmail.com>
2014-12-02 22:54:54 -0300

    adicionando tela de edição de perfil e mudando alguns views

--- src/com/nodoubts/core/Profile.java
+++ src/com/nodoubts/core/Profile.java
@@ -2,4 +2,12 @@ package com.nodoubts.core;
 
 public class Profile {
 
+	String name,dateOfBirth;
+	char sex;
+	
+	public Profile(String name, String dateOfBirth,char sex){
+		this.name= name;
+		this.dateOfBirth =dateOfBirth;
+		this.sex=sex;
+	}
 }
