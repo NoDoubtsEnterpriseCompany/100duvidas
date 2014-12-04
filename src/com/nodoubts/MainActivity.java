f73f42f6d8de21240a234559236d2f949d60c0b3
Jeymisson Oliveira <jeymissoncz@gmail.com>
2014-12-04 10:55:59 -0300

    Added Facebook's login MainFragment Class

--- src/com/nodoubts/MainActivity.java
+++ src/com/nodoubts/MainActivity.java
@@ -1,24 +1,37 @@
 package com.nodoubts;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+
+	private MainFragment mainFragment;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
+
+		if (savedInstanceState == null) {
+			mainFragment = new MainFragment();
+			getSupportFragmentManager()
+					.beginTransaction()
+					.add(android.R.id.content, mainFragment)
+					.commit();
+
+		} else {
+			mainFragment = (MainFragment) getSupportFragmentManager()
+					.findFragmentById(android.R.id.content);
+		}
+
 		Button registerBtn = (Button) findViewById(R.id.registerbtn);
 		registerBtn.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(MainActivity.this, RegisterUserActivity.class);
