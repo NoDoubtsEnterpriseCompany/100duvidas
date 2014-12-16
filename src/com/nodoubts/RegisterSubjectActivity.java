2d7b31fdaba330f10ad67c53e575dfa8db0e3d37
tiaraju <tiarajums@gmail.com>
2014-12-16 13:40:56 -0300

    adding register new subject activity

---
+++ src/com/nodoubts/RegisterSubjectActivity.java
@@ -0,0 +1,44 @@
+package com.nodoubts;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterSubjectActivity extends Activity {
+	
+	Button add;
+	EditText newSubjectName;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register_subject);
+		
+		add = (Button) findViewById(R.id.btn_add_subject);
+		newSubjectName = (EditText) findViewById(R.id.new_subject_name);
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.register_subject, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
--- src/com/nodoubts/core/Subject.java
+++ src/com/nodoubts/core/Subject.java
@@ -8,5 +8,17 @@ public class Subject implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
