ac822887dd210f8e89b1a1b5cd1b9f766a1a0b30
Jeymisson Oliveira <jeymissoncz@gmail.com>
2014-12-16 22:22:46 -0300

    Added facebook's profile pic to teacher profile

--- src/com/nodoubts/TeacherProfileActivity.java
+++ src/com/nodoubts/TeacherProfileActivity.java
@@ -1,13 +1,34 @@
 package com.nodoubts;
 
 
+import java.io.InputStream;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.entity.BufferedHttpEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.nodoubts.core.User;
@@ -17,12 +38,17 @@ public class TeacherProfileActivity extends Activity {
 	Button editBtn;
 	TextView name;
 	User user;
+	ImageView profilePicture;
+	ProgressBar waitSpinner;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_teacher_profile);
 		
+		waitSpinner = (ProgressBar) findViewById(R.id.singleSpinner);
+		profilePicture = (ImageView) findViewById(R.id.img_view_teacher);
 		editBtn = (Button) findViewById(R.id.edit_profilebtn);
 		name = (TextView) findViewById(R.id.name_text_view);
 		user = (User) getIntent().getSerializableExtra("user");
@@ -38,8 +64,13 @@ public class TeacherProfileActivity extends Activity {
 		    	startActivityForResult(editScreen,1);
 		    }
 		});
+		
+		SetProfilePicture setPictureTask = new SetProfilePicture();
+		setPictureTask.execute(user.getProfile().getProfilePic());
+
 	}
-	
+
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -71,5 +102,55 @@ public class TeacherProfileActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private class SetProfilePicture extends AsyncTask<String, Void, Bitmap> {
+			@Override
+			protected void onPreExecute(){
+				waitSpinner.setVisibility(View.VISIBLE);
+				profilePicture.setVisibility(View.INVISIBLE);
+			}
+			
+			protected Bitmap doInBackground(String... params) {
+				String picUrl = params[0];
+				Bitmap bitmap = null;
+				String picWidth = String.valueOf(profilePicture.getDrawable().getIntrinsicWidth());
+				String picHeight = String.valueOf(profilePicture.getDrawable().getIntrinsicHeight());
+				List<NameValuePair> queryParams = new LinkedList<NameValuePair>();
+				queryParams.add(new BasicNameValuePair("width", picWidth));
+				queryParams.add(new BasicNameValuePair("height", picHeight));
+				String encodedParams = URLEncodedUtils.format(queryParams, "utf-8");
+				String picQuery = picUrl.concat("?").concat(encodedParams);
+				Log.i("TeacherProfile", picQuery);
+				
+				try {
+			        URL url = new URL(picQuery);
+			        HttpGet httpRequest = null;
+
+			        httpRequest = new HttpGet(url.toURI());
+
+			        HttpClient httpclient = new DefaultHttpClient();
+			        HttpResponse response = (HttpResponse) httpclient
+			                .execute(httpRequest);
+
+			        HttpEntity entity = response.getEntity();
+			        BufferedHttpEntity b_entity = new BufferedHttpEntity(entity);
+			        InputStream input = b_entity.getContent();
+
+			        bitmap = BitmapFactory.decodeStream(input);
+
+			    } catch (Exception ex) {
+			    	ex.printStackTrace();
+			    }
+				return bitmap;
+			}
+			@Override
+			protected void onPostExecute(Bitmap result){
+				waitSpinner.setVisibility(View.INVISIBLE);
+				profilePicture.setVisibility(View.VISIBLE);
+				if(result!=null){
+					profilePicture.setImageBitmap(result);
+				}
+			}
 	}
 }
