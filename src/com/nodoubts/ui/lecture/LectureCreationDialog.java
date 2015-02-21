d1282a129b23470a2b3e59e4a0f3cff41b5ffdae
tiaraju <tiarajums@gmail.com>
2015-02-21 17:30:51 -0300

    fixing empty price bug

--- src/com/nodoubts/ui/lecture/LectureCreationDialog.java
+++ src/com/nodoubts/ui/lecture/LectureCreationDialog.java
@@ -8,63 +8,71 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.nodoubts.R;
 import com.nodoubts.core.User;
 
 public class LectureCreationDialog extends DialogFragment {
-	
+
 	EditText np;
 	EditText address;
-	
-	
-	public interface EditLectureListener{
+
+	public interface EditLectureListener {
 		void onFinishEditDialog(Double price, String address, User user);
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	}
-	
+
 	@Override
 	public void onStop() {
 		super.onStop();
 	}
-	
-    @Override
-    public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.create_lecture_dialog, container, false);
-        
-        np = (EditText) view.findViewById(R.id.price_picker);
-        
-        address = (EditText) view.findViewById(R.id.address);
 
-        Button okBtn = (Button) view.findViewById(R.id.create_lecture_ok);
-        Button cancelBtn = (Button) view.findViewById(R.id.create_lecture_cancel);
-        
-        okBtn.setOnClickListener(new OnClickListener() {
-			
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.create_lecture_dialog, container,
+				false);
+
+		np = (EditText) view.findViewById(R.id.price_picker);
+		np.requestFocus();
+
+		address = (EditText) view.findViewById(R.id.address);
+
+		Button okBtn = (Button) view.findViewById(R.id.create_lecture_ok);
+		Button cancelBtn = (Button) view
+				.findViewById(R.id.create_lecture_cancel);
+
+		okBtn.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				EditLectureListener activity = (EditLectureListener) getActivity();
-				activity.onFinishEditDialog(
-						Double.valueOf(np.getText().toString()), address.getText().toString(), (User) getArguments().get("user"));
-				dismiss();
+				double price = 0;
+				if (!np.getText().toString().isEmpty()) {
+					price = Double.valueOf(np.getText().toString());
+					activity.onFinishEditDialog(price, address.getText()
+							.toString(), (User) getArguments().get("user"));
+					dismiss();
+				}else{
+					Toast.makeText(getActivity().getApplicationContext(), "The price is invalid", Toast.LENGTH_SHORT).show();
+				}
 			}
 		});
-        
-        cancelBtn.setOnClickListener(new OnClickListener() {
-			
+
+		cancelBtn.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				dismiss();
 			}
 		});
-        
-        return view;
-    }
+
+		return view;
+	}
 
 }
