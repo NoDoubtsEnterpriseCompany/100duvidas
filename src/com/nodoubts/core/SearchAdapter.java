1cbd9c8e9cc51048ec2d696930fd7cfb2431f102
Jeymisson Oliveira <jeymissoncz@gmail.com>
2015-03-19 10:44:16 -0300

    commented subject img, TODO:Evaluate if this is gonna exist

--- src/com/nodoubts/core/SearchAdapter.java
+++ src/com/nodoubts/core/SearchAdapter.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nodoubts.R;
@@ -30,16 +29,16 @@ public class SearchAdapter<T extends SearchType> extends ArrayAdapter<T>{
 			convertView = LayoutInflater.from(getContext()).inflate(
 					R.layout.search_adapter, parent, false);
 		}
-		ImageView imageView = (ImageView) convertView.findViewById(R.id.search_obj_img);
+		//ImageView imageView = (ImageView) convertView.findViewById(R.id.search_obj_img);
 		TextView tv = (TextView) convertView.findViewById(R.id.search_obj_name);
 		SimpleDateFormat spf = new SimpleDateFormat("dd/MM/yyyy");
 		if (searchObj instanceof ScheduledLecture) {
 			ScheduledLecture lecture = (ScheduledLecture) searchObj;
-			imageView.setImageResource(R.drawable.lecture);
+			//imageView.setImageResource(R.drawable.lecture);
 			tv.setText(convertView.getResources().getText(R.string.lecture_at)+" "+spf.format(lecture.getDate()));
 		} else if (searchObj instanceof GroupLecture) {
 			GroupLecture groupLecture = (GroupLecture) searchObj;
-			imageView.setImageResource(R.drawable.group_lecture);
+			//imageView.setImageResource(R.drawable.group_lecture);
 			tv.setText(convertView.getResources().getText(R.string.group_lecture_at)+" "+spf.format(groupLecture.getDate()));
 		} else {
 			tv.setText(searchObj.getName());
